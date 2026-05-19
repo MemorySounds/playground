b11c510029efc17151746be4819f1a27fc432660
Robert Sahakyan <r.sahakyan1@gmail.com>
2026-05-19 18:39:59 +0200

    fix prev and next event order on mobile

--- .eleventy.js
+++ .eleventy.js
@@ -35,13 +35,15 @@ module.exports = function (eleventyConfig) {
   });
 
   // Prev/next navigation for event posts (collection is newest-first)
+  // "Previous" = up the list = newer event (lower index)
+  // "Next"     = down the list = older event (higher index)
   eleventyConfig.addFilter('prevEvent', (collection, url) => {
     const i = collection.findIndex((e) => e.url === url);
-    return i !== -1 && i < collection.length - 1 ? collection[i + 1] : null;
+    return i > 0 ? collection[i - 1] : null;
   });
   eleventyConfig.addFilter('nextEvent', (collection, url) => {
     const i = collection.findIndex((e) => e.url === url);
-    return i > 0 ? collection[i - 1] : null;
+    return i !== -1 && i < collection.length - 1 ? collection[i + 1] : null;
   });
 
   // Render markdown strings from YAML data in templates: {{ value | markdownify }}
